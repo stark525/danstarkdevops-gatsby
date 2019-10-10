b5d00e139864edde8f073f53e0a44dfd1fa7d10e
Dan Stark <dstark75@gmail.com>
2019-10-09 21:50:18 -0400

    More generic content

--- src/components/HeaderGeneric.js
+++ src/components/HeaderGeneric.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HeaderGeneric = (props) => (
     <header id="header">
-        <h1>Generic</h1>
-        <p>Ipsum dolor sit amet nullam</p>
+        <h1>Tell me more</h1>
+        <p>A collection of thoughts for you to get to know me better.</p>
     </header>
 )
 
--- src/pages/generic.js
+++ src/pages/generic.js
@@ -10,16 +10,16 @@ class Generic extends React.Component {
 
     return (
       <Layout>
-        <Helmet title="Generic Page Title" />
+        <Helmet title="What is DevOps to me" />
         <HeaderGeneric />
         <div id="main">
           <section id="content" className="main">
             <span className="image main"><img src={pic04} alt="" /></span>
-            <h2>Magna feugiat lorem</h2>
-            <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
-            <p>Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Consequat leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit enim turpis lorem ipsum dolor sit amet feugiat. Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet est velit quis lorem.</p>
-            <h2>Tempus veroeros</h2>
-            <p>Cep risus aliquam gravida cep ut lacus amet. Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat gravida placerat adipiscing in risus fusce vitae ac mi accumsan nunc in accumsan tempor blandit aliquet aliquet lobortis. Ultricies blandit lobortis praesent turpis. Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id. Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam. Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum condimentum semper commodo massa arcu.</p>
+            <h2>What is DevOps to me?</h2>
+            <p>Many people say DevOps is a culture, not a position. I agree to an extent.</p>
+            <p>Similar to Agile, it's most important to find a way of working where everyone feels productive, objectives are met, without compromising scalability or security.</p>
+            <h2>Communication is important</h2>
+            <p>I have always been passionate about technology, but also working with people. I have a Master's degree in School and Community counseling. It's critical to share what I know, through documentation and pairing with your team if possible. As this is a moonlighting position, there are some hurdles with asynchronous communication. I clearly and frequently update my progress and prioritize getting tasks "over the line."</p>
           </section>
         </div>
       </Layout>
